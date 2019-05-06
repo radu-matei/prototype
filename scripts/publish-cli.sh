a78ca93cbc02f71144f3c3f43010acd0c81877ba
Kent Rancourt <kent.rancourt@gmail.com>
2019-05-06 18:13:08 -0400

    improving pipeline selection criteria

--- scripts/publish-cli.sh
+++ scripts/publish-cli.sh
@@ -6,4 +6,8 @@ set -euox pipefail
 
 go get -u github.com/tcnksm/ghr
 
+set +x
+
+echo "Publishing CLI binaries for commit $DRAKE_SHA1"
+
 ghr -t $GITHUB_TOKEN -u krancour -r drake -c $DRAKE_SHA1 -delete $DRAKE_TAG ./bin/cli/
